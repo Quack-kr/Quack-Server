3ace7d753642258f9b40164e744182a207f13a14
changhyun9 <ckdgus1713@naver.com>
2025-03-27 15:38:26 +0900

    Feat: 사용자 저장 식당 모두 조회 기능 구현

--- src/main/java/org/quack/QUACKServer/repository/SavedRestaurantRepository.java
+++ src/main/java/org/quack/QUACKServer/repository/SavedRestaurantRepository.java
@@ -1,10 +1,13 @@
 package org.quack.QUACKServer.repository;
 
 
-import java.util.List;
-import org.quack.QUACKServer.domain.Restaurant;
 import org.quack.QUACKServer.domain.SavedRestaurant;
+import org.quack.QUACKServer.dto.restaurant.SavedRestaurantDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,5 +17,25 @@ public interface SavedRestaurantRepository extends JpaRepository<SavedRestaurant
 
     boolean existsByUser_UserIdAndRestaurant_RestaurantId(Long userId, Long restaurantId);
 
-    List<Restaurant> findAllByUser_UserId(Long userId);
+
+    @Query("SELECT new org.quack.QUACKServer.dto.restaurant.SavedRestaurantDto(" +
+            "       r.restaurantId, " +
+            "       r.representativeImage, " +
+            "       r.restaurantName, " +
+            "       r.category, " +
+            "       r.simpleDescription, " +
+            "       0L, " +
+            "       function('round', AVG(m.price)), " +
+            "       false, " +
+            "       r.latitude, " +
+            "       r.longitude" +
+            ") " +
+            "FROM SavedRestaurant sr " +
+            "JOIN sr.restaurant r " +
+            "LEFT JOIN r.menus m " +
+            "WHERE sr.user.userId = :userId " +
+            "GROUP BY r.restaurantId, r.representativeImage, r.restaurantName, r.category, " +
+            "         r.simpleDescription, r.latitude, r.longitude")
+    Page<SavedRestaurantDto> findSavedRestaurantsByUserId(@Param("userId") Long userId, Pageable pageable);
+
 }
