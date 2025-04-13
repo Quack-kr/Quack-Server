6b35653b1e087bcdc239d651bb1c219b9d7d553c
kwan2 <jgh9987@naver.com>
2025-04-14 00:13:58 +0900

    feat(init) : 프로젝트 외부 API 기본 샘플 적용

---
+++ src/main/java/org/quack/QUACKServer/global/config/stoarge/db/JpaConfig.java
@@ -0,0 +1,16 @@
+package org.quack.QUACKServer.global.config.stoarge.db;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * @author : jung-kwanhee
+ * @description :
+ * @packageName : org.quack.QUACKServer.global.config.infra
+ * @fileName : JpaConfig
+ * @date : 25. 4. 13.
+ */
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
