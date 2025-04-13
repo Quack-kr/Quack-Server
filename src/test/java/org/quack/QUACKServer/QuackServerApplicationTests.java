e59a5346be5dd8e5a3c04f945b192e40d489903f
kwan2 <jgh9987@naver.com>
2025-04-13 19:57:50 +0900

    feat(init) : 프로젝트 폴더 정상 이관

---
+++ src/test/java/org/quack/QUACKServer/QuackServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.quack.QUACKServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuackServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
