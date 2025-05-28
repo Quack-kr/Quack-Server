c36e56cfbdeb44b25c1387eb7a23f60b0cbc63c8
changhyun9 <ckdgus1713@naver.com>
2025-05-29 02:58:11 +0900

    feat: 식당 조회 유효성 검증 코드 추가 및 식당 정보 조회 기능구현

--- src/main/java/org/quack/QUACKServer/domain/restaurant/service/RestaurantService.java
+++ src/main/java/org/quack/QUACKServer/domain/restaurant/service/RestaurantService.java
@@ -1,2 +1,63 @@
-package org.quack.QUACKServer.domain.restaurant.service;public class RestaurantService {
+package org.quack.QUACKServer.domain.restaurant.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.quack.QUACKServer.domain.photos.domain.Photos;
+import org.quack.QUACKServer.domain.photos.dto.PhotosFileDto;
+import org.quack.QUACKServer.domain.photos.enums.PhotoEnum;
+import org.quack.QUACKServer.domain.photos.repository.PhotosRepository;
+import org.quack.QUACKServer.domain.photos.repository.PhotosS3Repository;
+import org.quack.QUACKServer.domain.restaurant.domain.Restaurant;
+import org.quack.QUACKServer.domain.restaurant.dto.response.GetRestaurantInfoResponse;
+import org.quack.QUACKServer.domain.restaurant.repository.RestaurantRepository;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RestaurantService {
+
+    private final RestaurantRepository restaurantRepository;
+    private final PhotosRepository photosRepository;
+    private final PhotosS3Repository photosS3Repository;
+
+    public GetRestaurantInfoResponse getRestaurantBasicInfo(Long restaurantId){
+        Optional<Restaurant> findRestaurant = restaurantRepository.findById(restaurantId);
+
+        Restaurant restaurant = findRestaurant.get();
+
+        List<String> restaurantCategory = new ArrayList<>();
+
+        for (String category : restaurantCategory) {
+            restaurantCategory.add(category);
+        }
+
+        Resource restaurantRepresentativePhoto = getRestaurantRepresentativePhoto(restaurantId);
+
+        return GetRestaurantInfoResponse.of(
+                restaurant.getRestaurantName(),
+                restaurant.getDetailAddress(),
+                restaurantCategory,
+                restaurantRepresentativePhoto
+        );
+    }
+
+    public Resource getRestaurantRepresentativePhoto(Long restaurantId) {
+
+        Photos photos = photosRepository.findFirstByTargetIdAndPhotoType(restaurantId, PhotoEnum.PhotoType.RESTAURANT.name())
+                .orElseThrow(() -> new IllegalArgumentException("이미지 정보 없음"));
+
+        return photosS3Repository.get(PhotosFileDto.builder().keyName(photos.getImageUrl()).build());
+
+
+    }
+
+    public boolean validateExistence(Long restaurantId){
+        return restaurantRepository.existsById(restaurantId);
+    }
+
+
 }
