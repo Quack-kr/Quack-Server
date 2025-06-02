079d4082e3e26d9bf4ac89e49b588c5c3f7398cc
changhyun9 <ckdgus1713@naver.com>
2025-06-02 19:44:45 +0900

    feat: 식당 상세페이지 조회 기능 구현

--- src/main/java/org/quack/QUACKServer/domain/restaurant/controller/RestaurantController.java
+++ src/main/java/org/quack/QUACKServer/domain/restaurant/controller/RestaurantController.java
@@ -4,9 +4,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.quack.QUACKServer.domain.restaurant.dto.request.SearchRestaurantsByKeywordRequest;
+import org.quack.QUACKServer.domain.restaurant.dto.response.GetRestaurantDetailInfo;
 import org.quack.QUACKServer.domain.restaurant.dto.response.SearchRestaurantsByKeywordResponse;
+import org.quack.QUACKServer.domain.restaurant.service.RestaurantService;
 import org.quack.QUACKServer.domain.restaurant.service.SearchRestaurantService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestaurantController {
 
     private final SearchRestaurantService searchRestaurantService;
+    private final RestaurantService restaurantService;
 
     /**
      * 검색 기능
@@ -32,5 +36,10 @@ public class RestaurantController {
     @GetMapping("/search/restaurants")
     public SearchRestaurantsByKeywordResponse searchSubtractRestaurants(@RequestBody(required = false) @Valid SearchRestaurantsByKeywordRequest request) {
         return searchRestaurantService.searchRestaurantByName(request);
+    }
+
+    @GetMapping("/{restaurantId}")
+    public GetRestaurantDetailInfo getRestaurantDetails(@PathVariable Long restaurantId) {
+        return restaurantService.getRestaurantDetailInfo(restaurantId);
     }
 }
