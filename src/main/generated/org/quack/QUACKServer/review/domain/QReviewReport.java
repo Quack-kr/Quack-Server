5bb439914147f38756f3981e964a0319c1b411f9
kwan2 <jgh9987@naver.com>
2025-06-03 17:05:15 +0900

    :sparkles: feat(리뷰_신고) : 리뷰 신고 기능 추가

---
+++ src/main/generated/org/quack/QUACKServer/review/domain/QReviewReport.java
@@ -0,0 +1,47 @@
+package org.quack.QUACKServer.review.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QReviewReport is a Querydsl query type for ReviewReport
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QReviewReport extends EntityPathBase<ReviewReport> {
+
+    private static final long serialVersionUID = -41745575L;
+
+    public static final QReviewReport reviewReport = new QReviewReport("reviewReport");
+
+    public final NumberPath<Long> customerUserId = createNumber("customerUserId", Long.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final BooleanPath isReported = createBoolean("isReported");
+
+    public final NumberPath<Long> restaurantId = createNumber("restaurantId", Long.class);
+
+    public final StringPath reviewContent = createString("reviewContent");
+
+    public final NumberPath<Long> reviewId = createNumber("reviewId", Long.class);
+
+    public QReviewReport(String variable) {
+        super(ReviewReport.class, forVariable(variable));
+    }
+
+    public QReviewReport(Path<? extends ReviewReport> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QReviewReport(PathMetadata metadata) {
+        super(ReviewReport.class, metadata);
+    }
+
+}
+
